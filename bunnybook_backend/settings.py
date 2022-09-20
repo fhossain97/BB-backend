e5a47321eb55f5941c3acc9df8dea06d46ba476e
fhossain <farhana.hossainse@gmail.com>
2022-09-19 21:26:45 -0400

    fixed2

--- bunnybook_backend/settings.py
+++ bunnybook_backend/settings.py
@@ -88,7 +88,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://bunnyback.herokuapp.com/bunnybook/"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://bunnyback.herokuapp.com"]
 
 ROOT_URLCONF = 'bunnybook_backend.urls'
 
