af0e94650d418b1ce7b67456d5d572df6fbc77f8
fhossain <farhana.hossainse@gmail.com>
2024-04-24 14:28:23 -0400

    remove db

--- bunnybook_backend/settings.py
+++ bunnybook_backend/settings.py
@@ -89,16 +89,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bunnybook_backend.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bunnybook',
-        'USER': 'bunnybookuser',
-        'PASSWORD': 'bunnybook',
-        'HOST': '',
-        'PORT' :'5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bunnybook',
+#         'USER': 'bunnybookuser',
+#         'PASSWORD': 'bunnybook',
+#         'HOST': '',
+#         'PORT' :'5432'
+#     }
+# }
 DATABASE_URL=env('DATABASE_URL')
 
 DATABASES = {
